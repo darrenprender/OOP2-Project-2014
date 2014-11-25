caeb7fba1c565975a6dad489c0251b4936a9fea6
darrenprender <djjones816@gmail.com>
2014-11-25 12:58:12 +0000

    Small changes made

--- CreateAProfile.java
+++ CreateAProfile.java
@@ -5,6 +5,6 @@ import javax.swing.*;
 public class CreateAProfile implements Serializable {
 	public static void main(String args[]) throws Exception
 		{
-			
+			JOptionPane.showInputDialog
 		}
 }
